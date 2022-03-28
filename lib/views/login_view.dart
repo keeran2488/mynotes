04c63c7e2d9554a7ab4aac3a3d0d6c47f6d72281
Kiran Chaudhary <chaudharyk456@gmail.com>
2022-03-28 19:21:07 +0545

    on sign in user is moved to notesview

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -60,6 +60,8 @@ class _LoginViewState extends State<LoginView> {
               try {
                 await FirebaseAuth.instance.signInWithEmailAndPassword(
                     email: email, password: password);
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/notes/', (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   devtools.log('User not found!');
