dbdb1646bdb3b2b2449dd4371be68099fc03a44b
Kiran Chaudhary <chaudharyk456@gmail.com>
2022-04-16 20:00:49 +0545

    Separated auth service from UI

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
